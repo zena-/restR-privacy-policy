4143503f296707494527a23310d3bacfecfd55b3
NErhabor <e.nosazena@gmail.com>
2026-06-27 09:33:17 -0400

    Initial Commit

---
+++ NYC restrooms/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  NYC restrooms
+//
+//  Created by nek erhabor on 6/27/26.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ NYC restrooms/NYC_restroomsApp.swift
@@ -0,0 +1,17 @@
+//
+//  NYC_restroomsApp.swift
+//  NYC restrooms
+//
+//  Created by nek erhabor on 6/27/26.
+//
+
+import SwiftUI
+
+@main
+struct NYC_restroomsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
